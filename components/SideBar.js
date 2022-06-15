68558d0c501d84073b3ef1df6b4555831fe34381
Alejandro Nicolas <alexnico80@gmail.com>
2022-06-15 15:24:14 +0200

    Routes in navbar

--- components/SideBar.js
+++ components/SideBar.js
@@ -9,9 +9,9 @@ const SideBar = () => {
   return (
     <div className="sticky top-0 left-0 flex flex-col w-20 h-screen m-0 bg-primary text-secondary drop-shadow-lg">
       <SideBarIcon icon={<BsFillHouseFill size="28" />} linkPath="/" />
-      <SideBarIcon icon={<HiTrendingUp size="28" />} linkPath="./market" />
-      <SideBarIcon icon={<BsPlusSquare size="28" />} linkPath="./create" />
-      <SideBarIcon icon={<FaRegUserCircle size="28" />} linkPath="./profile" />
+      <SideBarIcon icon={<HiTrendingUp size="28" />} linkPath="/market" />
+      <SideBarIcon icon={<BsPlusSquare size="28" />} linkPath="/create" />
+      <SideBarIcon icon={<FaRegUserCircle size="28" />} linkPath="/profile" />
     </div>
   );
 };
